cc959b06f0cc7d5879a5f167b8c6dde958f9bda1
Igrokov275 <igrokov275@gmail.com>
2019-11-11 18:28:54 +0400

    Оптимизация

--- js/main.js
+++ js/main.js
@@ -33,4 +33,18 @@ $(window).load(function() {
   });
 
   $('input[type="tel"]').mask("+7 (999) 999-99-99");
+
+  // Отложенная загрузка карты.
+  // Подгрузим когда пользователь долистает до Отзывы
+  let reviews = $('.reviews');
+  let reviewsTop = reviews.offset().top;
+  // $(window).scroll(function() {
+  $(window).bind('scroll', function() {
+    let windowTop = $(this).scrollTop();
+    if (windowTop > reviewsTop) {
+      $('#map').html('<script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A534e28a61a82b4c80620f69959b1d9d428474b966e9d91b5d479a7fe52562eac&amp;width=100%25&amp;height=410&amp;lang=ru_RU&amp;scroll=false"></script>');
+
+      $(window).unbind('scroll');
+    }
+  });
 });
